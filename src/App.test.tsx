6db37e0d4d0ed78232bdf86b179afd7593d392ae
akilanbandara <akilanbandara@gmail.com>
2022-05-16 00:07:13 +0530

    update test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { findByRole, findByTestId, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 /* 
@@ -14,11 +14,11 @@ queryAllBy...	|Return []	Return array	Return array	No
 findAllBy...	|Throw error	Return array	Return array	Yes
 */
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
 
 describe("First test check", () => {
 	test("Test whether true is true", () => {
@@ -27,22 +27,24 @@ describe("First test check", () => {
 });
 
 describe("Counter App", () => {
-	// Render any react component
-	const {queryByTestId, queryByRole, queryByText } = render(<App/>);
+	test("Is all elements are there? ", async () => {
+		const {findByTestId, queryByText } = render(<App/>);
 
-	// Get element which has "data-testid" attribute value
-	const count = queryByTestId("count-value"); 
-	// Get button element which has "name" attribute value
-	const incButton = queryByRole("button", {name: "/increment/"});
-	const decButton = queryByText("/decrement/");
+		const count = await findByTestId("count-value");
+		const decButton = queryByText("Decrement");
+		const incButton = queryByText("Increment");
 
-	test("Is all elements are there? ", () => {
 		expect(count).toBeInTheDocument();
 		expect(incButton).toBeInTheDocument();
 		expect(decButton).toBeInTheDocument();
 	});
 
-	test("Increament counter. ", () => {
+	test("Increament counter. ", async() => {
+		const {findByTestId, queryByText } = render(<App/>);
+
+		const count = await findByTestId("count-value");
+		const incButton = queryByText("Increment");
+
 		const currentValue = parseInt(count?.textContent || "");
 
 		if (incButton) {
@@ -52,15 +54,19 @@ describe("Counter App", () => {
 		expect(count).toHaveTextContent((currentValue + 1).toString());
 	});
 
-	test("Decreament counter. ", () => {
+	test("Decreament counter. ", async () => {
+		const {findByTestId, queryByText } = render(<App/>);
+
+		const count = await findByTestId("count-value");
+		const decButton = queryByText("Decrement");
+
 		const currentValue = parseInt(count?.textContent || "");
 
-		if (incButton) {
-			fireEvent.click(incButton);
+		if (decButton) {
+			fireEvent.click(decButton);
 		}
 
 		expect(count).toHaveTextContent((currentValue - 1).toString());
 	});
-
 });
 
